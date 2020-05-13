ca3fe21fe00ad49e7c39695ca4b357293020842f
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-13 23:20:49 +0000

    add function _mul

--- monty_math.c
+++ monty_math.c
@@ -32,3 +32,36 @@ void _sub(stack_t **stack, unsigned int line_number)
 
 	(*stack)->n = sub;
 }
+
+/**
+ * _sub - mul top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
+ */
+void _mul(stack_t **stack, unsigned int line_number)
+{
+    stack_t *tmp = *stack;
+	int mul = 1, i = 0;
+
+	if (tmp == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	while (tmp)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	if (stack == NULL || (*stack)->next == NULL || i <= 1)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	mul = (*stack)->next->n * (*stack)->n;
+	_pop(stack, line_number);
+
+	(*stack)->n = mul;
+}
