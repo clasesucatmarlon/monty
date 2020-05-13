ec28dbbd96c8d3f6865da34f3727df2a85d4a5ab
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-13 22:15:57 +0000

    replace function add

--- stack_func2.c
+++ stack_func2.c
@@ -29,28 +29,11 @@ void _swap(stack_t **stack, unsigned int line_number)
 
 void _add(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp = *stack;
-	int sum = 0, i = 0;
-
-	if (tmp == NULL)
+	if (stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
-	while (tmp)
-	{
-		tmp = tmp->next;
-		i++;
-	}
-
-	if (stack == NULL || (*stack)->next == NULL || i <= 1)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	sum = (*stack)->next->n + (*stack)->n;
+	(*stack)->next->n += (*stack)->n;
 	_pop(stack, line_number);
-
-	(*stack)->n = sum;
 }
