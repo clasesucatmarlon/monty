2c7df1f35ac95ac6cabf812296682d7c295893c2
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-13 17:18:48 +0000

    fix betty in file stack_func

--- stack_func.c
+++ stack_func.c
@@ -8,14 +8,15 @@ void _push(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
 {
 	stack_t *top;
 	(void)line_number;
+
 	top = malloc(sizeof(stack_t));
 	if (top == NULL)
 	{
-		printf(ERROR_MALLOC);
+		printf("Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	top->n = num;
+	top->n = push_arg;
 	top->next = *stack;
 	top->prev = NULL;
 	if (*stack != NULL)
