53feee1ad364d59f3402651108ef373f1d38e1df
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-13 22:57:31 +0000

    add function  _sub

---
+++ monty_math.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * _sub - sub top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
+ */
+void _sub(stack_t **stack, unsigned int line_number)
+{
+    stack_t *tmp = *stack;
+	int sub = 0, i = 0;
+
+	if (tmp == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	while (tmp)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	if (stack == NULL || (*stack)->next == NULL || i <= 1)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	sub = (*stack)->next->n - (*stack)->n;
+	_pop(stack, line_number);
+
+	(*stack)->n = sub;
+}
