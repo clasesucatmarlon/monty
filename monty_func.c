2bcc060cb2677f13ab01f062cacede8c13b67833
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-13 19:12:13 +0000

    replace function printf to fprintf

--- monty_func.c
+++ monty_func.c
@@ -19,7 +19,7 @@ void read_file(char *filename, stack_t **stack)
 
 	if (file == NULL)
 	{
-		printf("Error: Can't open file <%s>\n", filename);
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
 		exit(EXIT_FAILURE);
 	}
 
@@ -34,7 +34,7 @@ void read_file(char *filename, stack_t **stack)
 		s = get_op_func(line);
 		if (s == NULL)
 		{
-			printf("L<%d>: unknown instruction <%s>\n", line_count, line);
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_count, line);
 			exit(EXIT_FAILURE);
 		}
 		s(stack, line_count);
@@ -124,7 +124,7 @@ char *parse_line(char *line, stack_t **stack, unsigned int line_number)
 		}
 		else
 		{
-			printf("L<%d>: usage: push integer\n", line_number);
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
 			exit(EXIT_FAILURE);
 		}
 	}
