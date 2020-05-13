bb739a653b8db520fa176dace5a00c40c125133e
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-13 20:13:22 +0000

    Add prototype _add

--- stack_func2.c
+++ stack_func2.c
@@ -29,7 +29,7 @@ void _swap(stack_t **stack, unsigned int line_number)
 
 void _add(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL || (*stack)->next == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short", line_number);
 		exit(EXIT_FAILURE);
