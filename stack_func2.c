f58fe7c3341ce538593c7dcf264dcaa37429232a
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-13 19:52:07 +0000

    modific function _swap

--- stack_func2.c
+++ stack_func2.c
@@ -13,7 +13,7 @@ void _swap(stack_t **stack, unsigned int line_number)
 	runner = *stack;
 	if (runner->next == NULL)
 	{
-		fprintf(stderr, "L%d can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	tmp = runner->n;
