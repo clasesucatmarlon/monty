0234b3e251f27119e2f6213fc090894cc09da273
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-13 22:57:11 +0000

    add prototype  _sub

--- monty.h
+++ monty.h
@@ -47,8 +47,11 @@ void _push(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
-void _add(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
+
+/*Functions math*/
+void _sub(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
 
 void free_dlistint(stack_t *head);
 #endif
