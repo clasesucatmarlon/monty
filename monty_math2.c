7fb9ec60877fa33325fddd231bb67c91bb3d60b4
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-14 17:08:00 +0000

    add functions _pstr

--- monty_math2.c
+++ monty_math2.c
@@ -34,3 +34,24 @@ void _mod(stack_t **stack, unsigned int line_number)
 
 	(*stack)->n = modu;
 }
+
+/**
+ * _pstr - mod top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
+ */
+void _pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+
+	(void)line_number;
+
+	while (tmp)
+	{
+		if (tmp->n == 0)
+			break;
+		putchar(tmp->n);
+		tmp = tmp->next;
+	}
+	putchar('\n');
+}
