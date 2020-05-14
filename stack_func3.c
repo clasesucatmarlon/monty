c33af1c808d9dea1b27e0e748e4529f57b8e687c
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-14 23:01:18 +0000

    modify function _rotr

--- stack_func3.c
+++ stack_func3.c
@@ -51,7 +51,7 @@ void _rotl(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * _rotr - mod top of stack y second top stack
+ * _rotr - mod top of stack y second top stacks
  * @stack: pointer to lists for monty stack
  * @line_number: number of line opcode occurs on
  */
