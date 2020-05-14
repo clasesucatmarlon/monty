74c383eab724e18eb27dead504ac0193be263f74
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-14 18:46:28 +0000

    change name file

---
+++ stack_func3.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+
+/**
+ * _pstr - mod top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
+ */
+void _pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+	int c = 0;
+
+	(void)line_number;
+
+
+	while (tmp)
+	{
+		c = tmp->n;
+		if (c == 0 || _isalpha(c) == 0)
+			break;
+		putchar(c);
+		tmp = tmp->next;
+	}
+	putchar('\n');
+}
