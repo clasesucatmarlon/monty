036fa9bfdb3c9d9bbba652984492430db9094666
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-14 16:50:42 +0000

    add _mod to prototype functions

--- monty.h
+++ monty.h
@@ -57,6 +57,7 @@ void _sub(stack_t **stack, unsigned int line_number);
 void _add(stack_t **stack, unsigned int line_number);
 void _mul(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
 
 void free_dlistint(stack_t *head);
 void dobfree(stack_t **stack);
