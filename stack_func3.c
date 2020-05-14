e1d842f2781689fa04c1c9245074449a8dd9801a
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-14 22:54:22 +0000

    add _rotr

--- stack_func3.c
+++ stack_func3.c
@@ -49,3 +49,31 @@ void _rotl(stack_t **stack, unsigned int line_number)
 
 	runner->n = aux1;
 }
+
+/**
+ * _rotr - mod top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
+ */
+void _rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *runner = *stack;
+
+	int aux1 = 0;
+
+	if (!line_number || !stack || !*stack || !(*stack)->next)
+		return;
+
+	while (runner->next)
+		runner = runner->next;
+	
+	aux1 = runner->n;
+	
+	while (runner->prev)
+	{
+		runner = runner->prev;
+		runner->next->n = runner->n;
+	}
+
+	runner->n = aux1;
+}
